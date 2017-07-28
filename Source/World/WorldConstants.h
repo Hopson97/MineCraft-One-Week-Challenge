b11b8b46fae4f53784b3b99e65795670ca2b2833
Hopson97 <mhopson@hotmail.co.uk>
2017-07-28 13:16:39 +0100

    Add world constants header

---
+++ Source/World/WorldConstants.h
@@ -0,0 +1,8 @@
+#ifndef WORLDCONSTANTS_H_INCLUDED
+#define WORLDCONSTANTS_H_INCLUDED
+
+constexpr int   CHUNK_SIZE      = 16,
+                CHUNK_AREA      = CHUNK_SIZE * CHUNK_SIZE,
+                CHUNK_VOLUME    = CHUNK_SIZE * CHUNK_SIZE * CHUNK_SIZE;
+
+#endif // WORLDCONSTANTS_H_INCLUDED
