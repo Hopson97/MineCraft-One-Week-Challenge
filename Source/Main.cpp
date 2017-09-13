f8059cf8d240ad430524e0435fb97af5f900d53a
Hopson97 <mhopson@hotmail.co.uk>
2017-09-13 12:56:26 +0100

    Add intro text

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -55,6 +55,28 @@ int main()
         std::cerr << "Error: Could not find config.txt file! Using defaults.\n";
     }
 
+    std::cout   << "\n\n============WELCOME TO MINECRAFT IN A WEEK============\n"
+                << "This is a very simple Minecraft clone (mostly) created in one week\n\n"
+                << "Because it was made in a week, some major features were missing:\n"
+                << "-Level loading (Chunks do not save! So, I wouldn't commit to a build here for now)\n"
+                << "-Caves\n"
+                << "-Mobs\n"
+                << "-Crafting\n\n"
+                << "However, I did manage to implement some other important things:\n"
+                << "-World generation\n"
+                << "-Simple resource collection\n"
+                << "-Biomes\n"
+                << "-Collision detection\n"
+                << "-Block breaking/ placing\n\n"
+                << "For now, the game it pretty much entirely a community-driven project.\n"
+                << "Source code can be found at https://github.com/Hopson97/MineCraft-One-Week-Challenge\n"
+                << "Feel free to contribute!\n\n"
+                << "Please view 'controls.txt for controls!\n\n"
+                << "============Press enter to begin ============\n";
+
+    std::cin.ignore();
+    std::cout << "Loading game...\n";
+
     Application app(config);
     app.runLoop();
 }
