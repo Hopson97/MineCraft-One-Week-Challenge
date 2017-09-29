a0c492828f406719e8d7089bf943c970e91a1dc4
NT-Bourgeois-Iridescence-Technologies <19nbourgeois_68@perrysburgstudents.net>
2017-09-29 18:07:59 -0400

    Fixed issue 42

--- Source/Shaders/ShaderLoader.cpp
+++ Source/Shaders/ShaderLoader.cpp
@@ -43,8 +43,8 @@ namespace
 GLuint loadShaders(const std::string& vertexShader,
                  const std::string& fragmentShader)
 {
-    auto vertexSource   = getFileContents("Shaders/" + vertexShader     + ".glsl");
-    auto fragmentSource = getFileContents("Shaders/" + fragmentShader   + ".glsl");
+    auto vertexSource   = getFileContents("Shaders/" + vertexShader     + ".vert");
+    auto fragmentSource = getFileContents("Shaders/" + fragmentShader   + ".frag");
 
     auto vertexShaderID     = compileShader(vertexSource.c_str(),   GL_VERTEX_SHADER);
     auto fragmentShaderID   = compileShader(fragmentSource.c_str(), GL_FRAGMENT_SHADER);
