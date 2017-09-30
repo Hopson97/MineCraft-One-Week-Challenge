0b089e5e54656e5dc335b3f8d8a01d425d3be10e
NT-Bourgeois-Iridescence-Technologies <19nbourgeois_68@perrysburgstudents.net>
2017-09-29 23:25:01 -0400

    Fixed bug of tick speed.

--- Source/Tick/TickManager.cpp
+++ Source/Tick/TickManager.cpp
@@ -17,7 +17,7 @@ void TickManager::run(){
     while(g_window->isOpen()){
         time = clock.getElapsedTime();
         
-        if((int)(time.asMilliseconds() / 50) > m_tickTime){
+        if((int)(time.asMilliseconds() / 5) > m_tickTime){
             if(!m_tickObjects.empty()){
                 for(TickObject* t : m_tickObjects){
                     TickObject* g = t;
@@ -26,7 +26,7 @@ void TickManager::run(){
             }
         }
 
-        m_tickTime = (time.asMilliseconds() / 50);
+        m_tickTime = (time.asMilliseconds() / 5);
 
         if(m_tickTime > 23999){
             m_tickTime = 0;
