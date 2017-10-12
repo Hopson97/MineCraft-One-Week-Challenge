3d4e9b1a043b6ed424cc9a5320cdee7de0001aa3
NT-Bourgeois-Iridescence-Technologies <19nbourgeois_68@stu-2019-040435.local>
2017-10-12 17:01:55 -0400

    Modified texture mapping.

--- Source/Texture/BasicTexture.cpp
+++ Source/Texture/BasicTexture.cpp
@@ -16,10 +16,10 @@ void BasicTexture::loadFromImage(const sf::Image& i)
                  0, GL_RGBA, GL_UNSIGNED_BYTE, i.getPixelsPtr());
 
     glGenerateMipmap(GL_TEXTURE_2D);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 1);
 
 }
