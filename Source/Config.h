096cdb8e2decf77e0d13e2185974cbd65e5cf9a6
UglySwedishFish <30728513+UglySwedishFish@users.noreply.github.com>
2018-08-06 12:42:38 +0200

    Updated Config.h
    Made code more in tune with what hopson wanted

--- Source/Config.h
+++ Source/Config.h
@@ -8,10 +8,6 @@ struct Config
     bool isFullscreen   = false;
     int renderDistance  = 16;
     int fov             = 90;
-    float gamma         = 1.3f;
-    float brightness    = 1.15f;
-    float contrast      = 1.1f;
-    bool postProcess    = true;
 };
 
 #endif // CONFIG_H_INCLUDED
