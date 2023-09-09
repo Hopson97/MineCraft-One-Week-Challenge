166d1c7b49e31f20fac65b079e96eab98b2b2df6
DreamHollow <404errorman@gmail.com>
2023-09-09 02:39:24 -0400

    GeneralMaths.h: Update to inclusion

--- Source/Maths/GeneralMaths.h
+++ Source/Maths/GeneralMaths.h
@@ -1,5 +1,5 @@
 #ifndef GENERALMATHS_H_INCLUDED
-#define GENERALMATHS_H_INCLUDED
+#define GENERALMATHS_H_INCLUDED 1
 
 float bilinearInterpolation(float bottomLeft, float topLeft, float bottomRight,
                             float topRight, float xMin, float xMax, float zMin,
